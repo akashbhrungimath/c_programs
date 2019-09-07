eb00b2d9b16deb6a0c557a8534485e4f33a3cc26
Akash <akash.ee19@bmsce.ac.in>
2019-09-07 07:14:25 +0000

    final commit

--- temperature.c
+++ temperature.c
@@ -2,9 +2,9 @@
 int main()
 {
      float c,f;
-     c=((f-32)*5)/9;
      printf("enter the temparature in degree ferenheit=");
      scanf("%f",&f);
+     c= ((f-32)*5)/9;
      printf("temperature in degree celcius is=%f",c);
      return 0;
 }
