834a96769386afc57a08ac95695faaa3d0646907
Akash <akash.ee19@bmsce.ac.in>
2019-09-07 07:01:32 +0000

    initial commit

---
+++ temperature.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+     float c,f;
+     c=((f-32)*5)/9;
+     printf("enter the temparature in degree ferenheit=");
+     scanf("%f",&f);
+     printf("temperature in degree celcius is=%f",c);
+     return 0;
+}
