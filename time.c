03c71e271d5fcf2496d848a73dd85921fe6467ef
Akash <akash.ee19@bmsce.ac.in>
2019-09-07 06:36:34 +0000

    final commit

--- time.c
+++ time.c
@@ -7,13 +7,13 @@ int main()
      scanf("%d",&h);
      printf("enter how many minutes=");
      scanf("%d",&m);
-     time(h,m);
+     t=time(h,m);
      printf("total time in minutes is=%d",t);
      return 0;
 }
 int time(int h,int m)
 {
      int t;
-     t=h*60+m;
-     return t;
+     t=(h*60)+m;
+     return t   ;
 }
