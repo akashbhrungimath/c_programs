78406640a51d7cbfe4145aad47032b61d01a4c5c
Akash <akash.ee19@bmsce.ac.in>
2020-02-07 13:02:23 +0000

    fraction.c super commit

--- fraction.c
+++ fraction.c
@@ -8,7 +8,7 @@ typedef struct frac FRAC;
 FRAC input(int);
 FRAC calc(FRAC,FRAC);
 int gcd(int,int);
-FRAC reduction(FRAC,int);
+FRAC reduction(FRAC);
 void output(FRAC,FRAC,FRAC);
 FRAC input(int i)
 {
@@ -23,6 +23,15 @@ FRAC calc(FRAC f1,FRAC f2)
     float a,b;
     f.num=(f1.num*f2.den)+(f2.num*f1.den);
     f.den=(f1.den*f2.den);
+    f=reduction(f);
+    return f;
+}
+FRAC reduction(FRAC f)
+{
+	int g;
+    g=gcd(f.num,f.den);
+    f.num=f.num/g;
+    f.den=f.den/g;
     return f;
 }
 int gcd(int a,int b)
@@ -36,12 +45,6 @@ int gcd(int a,int b)
     }
 	return a;
 }
-FRAC reduction(FRAC f,int g)
-{
-    f.num=f.num/g;
-    f.den=f.den/g;
-    return f;
-}
 void output(FRAC f3,FRAC f2,FRAC f1)
 {
     printf("sum of the (%d/%d) and (%d/%d) is=%d/%d\n",f1.num,f1.den,f2.num,f2.den,f3.num,f3.den);
@@ -53,8 +56,6 @@ int main()
     f1=input(1);
     f2=input(2);
     f3=calc(f1,f2);
-    g=gcd(f3.num,f3.den);
-    f3=reduction(f3,g);
     output(f3,f2,f1);
     return 0;
 }
