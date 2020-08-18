282e1500498b4b2bff2053fdfae906ec3b157c51
Akash <akash.ee19@bmsce.ac.in>
2020-08-18 10:25:54 +0000

    partion.c second commit(incomplete)

--- partion.c
+++ partion.c
@@ -1,11 +1,16 @@
 #include<stdio.h>
+int input();
+void get_array(int ,int *);
+void compute(int ,int *,int *);
+void output(int ,int *);
 int main()
 {
 	int n;
 	n=input();
-	int a[n];
+	int a[n],b[n];
 	get_array(n,a);
-	compute(n,a);
+	compute(n,a,b);
+	output(n,b);
 }
 int input()
 {
@@ -22,8 +27,32 @@ void get_array(int n,int a[n])
 		scanf("%d",&a[i]);
 	}
 }
-void compute(int n,int a[n])
+void compute(int n,int a[n],int b[n])
 {
-	int fn=a[0];
-	
+	int p=a[0];
+	b[n]=(0),b[0]=a[0];
+	for(int i=0;i<n;i++)
+	{
+		for(int j=0;j<n;j++)
+		{
+			if(p<a[j])
+			{
+				for(int k=j;k>=0;k--)
+					b[k]=b[k-1];
+				b[0]=a[i];
+			}
+			if(p>a[j])
+			{
+				for(int k=n-1;k>i;k--)
+					b[k]=b[k-1];
+				b[i]=a[i];
+			}
+		}
+	}
+}
+void output(int n,int b[n])
+{
+	for(int i=0;i<n;i++)
+		printf("%d",b[i]);
+	printf("\n");
 }
