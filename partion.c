2cd03b26053eaf37abab326294f2898bf14cae2e
Akash <akash.ee19@bmsce.ac.in>
2020-08-27 10:21:53 +0000

    partion.c improved_commit(2)

--- partion.c
+++ partion.c
@@ -3,10 +3,7 @@ int input();
 void get_array(int ,int *);
 void compute(int ,int *);
 int count_no_of_small_nos(int,int,int *);
-int count_no_of_high_nos(int,int,int *);
-void collect_the_smalls(int,int,int *,int *);
-void collect_the_highs(int,int,int *,int *);
-void my_assemble(int,int *,int,int,int,int *,int *);
+void my_append(int,int *,int,int);
 void output(int ,int *);
 int main()
 {
@@ -34,70 +31,35 @@ void get_array(int n,int a[n])
 }
 void compute(int n,int a[n])
 {
-	int pivot=a[0],s,h;
+	int pivot=a[0],s;
 	s=count_no_of_small_nos(pivot,n,a);
-	int b[s];
-	h=count_no_of_high_nos(pivot,n,a);
-	int c[h];
-	collect_the_smalls(n,pivot,a,b);
-	collect_the_highs(n,pivot,a,c);
-	my_assemble(n,a,pivot,s,h,b,c);
+	my_append(n,a,pivot,s);
 }
 int count_no_of_small_nos(int pivot,int n,int a[n])
 {
 	int s=0;
-	for(int i=0;i<n;i++)
+	for(int i=1;i<n;i++)
 	{
-		if(a[i]<pivot)
+		if(a[i]<=pivot)
 			s++;
 	}
 	return s;
 }
-int count_no_of_high_nos(int pivot,int n,int a[n])
+void my_append(int n,int a[n],int pivot,int s)
 {
-	int h=0;
-	for(int i=0;i<n;i++)
+	int j=0,k=n-1,l;
+	for(int i=1;i<=s;i++)
 	{
-		if(a[i]<pivot)
-			h++;
-	}
-	return h;
-}
-void collect_the_smalls(int n,int pivot,int a[n],int b[n])
-{
-	int k=0;
-	for(int i=0;i<n;i++)
-	{
-		if(a[i]<pivot)
-		{
-			b[k++]=a[i];
-		}
-	}
-}
-void collect_the_highs(int n,int pivot,int a[n],int c[n])
-{
-	int k=0;
-	for(int i=0;i<n;i++)
-	{
+		if(a[i]<=pivot)
+			a[i-1]=a[i];
 		if(a[i]>pivot)
 		{
-			c[k++]=a[i];
+			l=a[k];
+			a[k--]=a[i];
+			a[i-1]=l;
 		}
 	}
-}
-void my_assemble(int n,int a[n],int pivot,int s,int h,int b[s],int c[h])
-{
-	int j=0;
-	for(int i=0;i<s;i++)
-	{
-		a[i]=b[i];
-	}
-	a[s++]=pivot;
-	for(int i=s;i<n;i++)
-	{
-		a[i]=c[j];
-		j++;
-	}
+	a[s]=pivot;
 }
 void output(int n,int a[n])
 {
