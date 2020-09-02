f3c4eb7119c47a009893e17e73966273afee562b
Akash <akash.ee19@bmsce.ac.in>
2020-09-02 09:27:07 +0000

    rts_of_qdeqns.py improved commit(3)

--- rts_of_qdeqns.py
+++ rts_of_qdeqns.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def my_input():
 	a=int(input("Enter the value of a in the quadratic equation="))
 	b=int(input("Enter the value of b in the quadratic equation="))
@@ -5,26 +7,26 @@ def my_input():
 	return a,b,c
 	
 def my_rts(a,b,c):
-	z=sqrt(b**2-4*a*c)
+	z=b**2-4*a*c
 	if  z>0:
-		x=(-b+z)/2*a
-		y=(-b-z)/2*a
+		x=(-b+sqrt(z))/2*a
+		y=(-b-sqrt(z))/2*a
 	elif z<0:
 		x=(-b/2*a)
 		y=(z/2*a)
-	else
+	else:
 		x=-b/2*a
 		y=-b/2*a
 	return x,y,z
 	
 def my_output(x,y,z,a,b,c):
-	if z<0
-		print("the roots of the quadratic equation (%d)x^2 + (%d)x + (%d) are (%d+i%d) and (%d-i%d)"%(a,b,c,x,y,x,y))
-	else 
-		print("the roots of the quadratic equation (%d)x^2 + (%d)x + (%d) are %d and %d"%(a,b,c,x,y))
+	if z<0:
+		print("the roots of the quadratic equation (%d)x^2 + (%d)x + (%d) are (%d+i(%d)) and (%d-i(%d))"%(a,b,c,x,y,x,y))
+	else:
+		print("the roots of the quadratic equation (%d)x^2 + (%d)x + (%d) are %f and %f"%(a,b,c,x,y))
 	
 def main():
-	a,b,c=,my_input()
+	a,b,c=my_input()
 	x,y,z=my_rts(a,b,c)
 	my_output(x,y,z,a,b,c)
 	
