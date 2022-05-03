6a8d6d0642c722c700220649d4c97cad13310c5c
akashbhrungimath <94945180+akashbhrungimath@users.noreply.github.com>
2022-05-03 19:24:05 +0530

    Update qsort_for_2d_array.c
    improved commit 2

--- qsort_for_2d_array.c
+++ qsort_for_2d_array.c
@@ -2,37 +2,63 @@
 #include<stdlib.h>
 #include<string.h>
 int get_input();
-void get_array(int ,char (*)[]);
-int compare(const void *,const void *);
-void display(int,char (*)[]);
+void get_array(int ,int,char (*)[]);
+void bubblesort(void**,int,int,int (*fun)(const void * ,const void *));
+int my_fun(const void *,const void *);
+void display(int,int,char (*)[]);
 int get_input()
 {
 	int n;
-	printf("enter the no. of strings you want to enter=");
 	scanf("%d",&n);
 	return n;
 }
-void get_array(int n,char a[n][100])
+void get_array(int n,int length,char a[n][length])
 {
-	printf("enter the strings=\n");
 	for(int i=0;i<n;i++)
-		scanf(" %s\n",a[i]);
+		scanf(" %s\n",*(a+i));
 }
-void display(int n,char a[n][100])
+void bubblesort(void **a,int no_names,int size_of_name,int (*fun)(const void *,const void *))
+{
+	int flag;
+	void *temp;
+	temp=(void*)malloc(size_of_name);
+	for(int i=0;i<no_names-1;i++)
+	{
+		for(int j=i+1;j<no_names;j++)
+		{
+			flag=fun((const void*)a+(i*size_of_name),(const void*)a+(j*size_of_name));
+			if(flag>0)
+			{
+				memcpy(temp,(const void*)a+(i*size_of_name),size_of_name);
+				memcpy((void*)a+(i*size_of_name),(const void*)a+(j*size_of_name),size_of_name);
+				memcpy((void*)a+(j*size_of_name),(const void*)temp,size_of_name);
+			}	
+		}
+	}
+	free(temp);
+}
+int my_fun(const void *a,const void *b)
+{
+	return strcmp((char*)a,(char*)b);
+}
+void display(int n,int length,char a[n][length])
 {
 	printf("the sorted list is=\n");
 	for(int i=0;i<n;i++)
-	{
 		printf("%s\n",a[i]);
-	}
 }
 int main()
 {
 	int no_names,name_length,q;
+	printf("enter the no. of strings you want to enter=");
 	no_names=get_input();
-	char a[no_names][100];
-	get_array(no_names,a);
-	qsort(a,no_names,100,(int (*) (const void *,const void *))strcmp);
-	display(no_names,a);
+	printf("enter the length of lenghtiest name in the list=");
+	name_length=get_input();
+	name_length++;
+	char a[no_names][name_length];
+	printf("enter the strings=\n");
+	get_array(no_names,name_length,a);
+	bubblesort((void**)a,no_names,sizeof(a[0]),&my_fun);
+	display(no_names,name_length,a);
 	return 0;
 }
