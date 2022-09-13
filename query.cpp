b8c1eeef791c7d9f4ce26127ee8c7b16a706d6e2
Akash <94945180+akashbhrungimath@users.noreply.github.com>
2022-09-13 18:50:03 +0530

    Create query.cpp

---
+++ query.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void compile(int querry_count,char **querry,int student1_count,int *student1,int student2_count,int student2)
+{
+  
+}
+int main()
+{
+  int querry_count=3,student1_count=3,student2_count=3,student1[student1_count],student2[student2_count];
+  char** querry={"Friend","Friend","Total"};
+  student1={1,2,1};
+  student2={2,3,4};
+  compile(querry_count,querry,student1_count,student1,student2_count,student2);
+  return 0;
+}
