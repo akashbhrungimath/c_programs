1453dd117c5288b37e1affea6d96176612aaff5c
Akash <akashbhrungimath@gmail.com>
2023-09-06 13:11:22 +0530

    first commit

---
+++ Program.java
@@ -0,0 +1,10 @@
+//import java.lang.*;
+class Program{
+    public static void main(String[] args){
+        String a = new String("Akash");
+        System.out.println(a.charAt(1));
+        char[] ab = a.toCharArray();
+        System.out.println("After converting to Array: ");
+        System.out.println(ab[2]);
+    }
+}
